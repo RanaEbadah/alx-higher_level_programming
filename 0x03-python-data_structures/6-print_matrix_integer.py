36cc47bc62698a02f8f06cc07eb403f41334c128
Rana Ebadah <ranaebadah@hotmail.com>
2023-09-12 03:37:36 +0400

    G

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,4 +9,4 @@ def print_matrix_integer(matrix=[[]]):
                 else:
                     print("{:d}".format(element), end=" ")
                 i += 1
-            print("$")
+            print("")
