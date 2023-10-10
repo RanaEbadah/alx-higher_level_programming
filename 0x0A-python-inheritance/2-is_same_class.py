8b757a3fe0e4e66827ac11cdc8021145d3327969
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-10 04:21:08 +0400

    task0

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,4 +9,4 @@ def is_same_class(obj, a_class):
         a_class: the parent class
     Returns:
         bool: True or False.'''
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
