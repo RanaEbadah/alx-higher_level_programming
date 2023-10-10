aecb2414e94c39ff515b046e0bcae090a7b5e1b8
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-10 04:32:38 +0400

    task3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-'''Module for isinstance function.'''
+'''Module for is_kind_of_class method.'''
 
 
 def is_kind_of_class(obj, a_class):
-    '''isinstance function.
-    Args:
-        obj: the object we have to look up it.
-        a_class: the parent class
-    Returns:
-        bool: True or False.'''
-    def is_kind_of_class(obj, a_class):
-        return isinstance(obj, a_class)
+    '''Determines if an object is a subclass of a class.'''
+    return isinstance(obj, a_class)
