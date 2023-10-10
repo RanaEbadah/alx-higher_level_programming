a9fd5a140d3c5b470e98397c5bd747458c83166a
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-10 04:25:39 +0400

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module for isinstance function.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''isinstance function.
+    Args:
+        obj: the object we have to look up it.
+        a_class: the parent class
+    Returns:
+        bool: True or False.'''
+    def is_kind_of_class(obj, a_class):
+        return isinstance(obj, a_class)
