8a6fccb3a2f7f8d9aabba2e9bd44c4ff77a7b083
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-12 00:48:13 +0400

    g

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     '''A function returns an object represented by a JSON string'''
-    with open(filename,'w') as f:
+    with open(filename, 'w') as f:
         f.write(json.dumps(my_obj))
