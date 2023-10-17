f713d970f893418498fa11171d1a8f672d5d2362
Rana Ebadah <ranaebadah@hotmail.com>
2023-10-18 01:28:31 +0400

    h

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,19 @@ class Square(Rectangle):
         '''str override'''
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y, self.height)
+
+    @property
+    def size(self):
+        '''width getter'''
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        '''width setter'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
