16abbe7c3e4f832359077b29ae5bb7da2e30fbf9
Rana Ebadah <ranaebadah@hotmail.com>
2023-12-21 22:31:16 +0400

    g

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""lists first State from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State.id).filter(
+        State.name == sys.argv[4]).order_by(State.id)
+    if (states):
+        for state in states:
+            tuple_elements = [str(element) for element in state]
+            result = ", ".join(tuple_elements)
+
+            print(result)
+    else:
+        print("Not found")
