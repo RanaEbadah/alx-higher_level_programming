330581b7b24239e84fd58efd19b1a5c65d19dd0c
Rana Ebadah <ranaebadah@hotmail.com>
2023-12-21 22:45:03 +0400

    g

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""lists first State from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newState = State(name="Louisiana")
+    session.add(newState)
+
+    session.commit()
