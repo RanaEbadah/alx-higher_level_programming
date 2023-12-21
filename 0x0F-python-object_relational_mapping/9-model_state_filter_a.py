521e447cc8c1eecb5efbfbda89d826f97fe4f2c2
Rana Ebadah <ranaebadah@hotmail.com>
2023-12-21 22:18:40 +0400

    fF

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,7 +5,7 @@ import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import func
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    fState = session.query(State).first()
-    if fState:
-        print(fState.id, fState.name, sep=": ")
-    else:
-        print("Nothing")
+    states = session.query(State).filter(func.REGEXP_LIKE(
+        State.name, r".*a.*")).order_by(State.id)
+    for state in states:
+        print(state.id, state.name, sep=": ")
