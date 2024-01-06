18db372b24cb6a32f2f57a8535d53c69091a4da4
Rana Ebadah <ranaebadah@hotmail.com>
2024-01-07 03:58:31 +0400

    f

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #GET request to the URL, and displays the body of the response.
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -sX POST "$1"
