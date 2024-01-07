8c6966dd6eae3791615f95f50b147a149f64dc1b
Rana Ebadah <ranaebadah@hotmail.com>
2024-01-07 04:41:36 +0400

    f

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #GET request to the URL, and displays the body of the response.
-curl -sX POST -H "Content-Type: application/json" @"$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
