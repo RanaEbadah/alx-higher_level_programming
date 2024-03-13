3a2168f2b168d1e8123472e19f8bbc032b20b69e
Rana Ebadah <ranaebadah@hotmail.com>
2024-03-13 22:40:46 +0400

    f

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-if (!isNaN(process.argv[2]))
-    console.log('My number: ' + process.argv[2]);
-else
-    console.log('Not a number');
+if (!isNaN(process.argv[2])) { console.log('My number: ' + process.argv[2]); } else { console.log('Not a number'); }
