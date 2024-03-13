000a3685e444e5cc092dd6c7ebd2cb2da46c892d
Rana Ebadah <ranaebadah@hotmail.com>
2024-03-13 21:07:07 +0400

    g

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorialCalc (n) {
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  if (n < 0) {
+    return (-1);
+  }
+  return (n * factorialCalc(n - 1));
+}
+
+console.log(factorialCalc(Number(process.argv[2])));
